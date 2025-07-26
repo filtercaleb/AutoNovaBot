faae8934292e726627821aef008bd7ff42386969
Caleb <filtercaleb@gmail.com>
2025-07-26 08:47:51 +0200

    Working gripper

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WristandGripper.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WristandGripper.java
@@ -8,15 +8,17 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp
 public class WristandGripper extends LinearOpMode {
+    double waitTime = 1.0;
+    double wristspeed = 0.001;
+    boolean isBusyGrabbing;
     ElapsedTime runtime;
     DigitalChannel sensor;
     Servo handServo;
     Servo wristServo;
     double open = 1.0;
-    double close = 0.0;
+    double close = 0.4;
     int stage = 1;
 
-    long stageStart = 0;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -33,10 +35,15 @@ public class WristandGripper extends LinearOpMode {
         // START
         waitForStart();
 
+        handServo.setPosition(open);
+        isBusyGrabbing = false;
 
         while (opModeIsActive()) {
 
             if (gamepad1.x) {
+                isBusyGrabbing = true;
+            }
+            if (isBusyGrabbing) {
                 grabTheBlock();
             }
 
@@ -45,92 +52,72 @@ public class WristandGripper extends LinearOpMode {
             }
 
             if (gamepad1.a) {
-                double newPosition = wristServo.getPosition() + 0.001;
+                double newPosition = wristServo.getPosition() + wristspeed;
                 wristServo.setPosition(newPosition);
             }
 
             if (gamepad1.b) {
-                double newPosition = wristServo.getPosition() - 0.001;
+                double newPosition = wristServo.getPosition() - wristspeed;
                 wristServo.setPosition(newPosition);
             }
+
         }
-        telemetry.addLine("stopped");
-        telemetry.update();
     }
 
     public void grabTheBlock() throws InterruptedException {
 
-        telemetry.addData("Stage: ", stage);
         if (runtime == null) {
             runtime = new ElapsedTime();
         }
 
-        telemetry.addLine("Grabbing block");
         if (stage == 1) {
 
-            telemetry.addLine("Stage 1");
             handServo.setPosition(close);
-            telemetry.addData("the seconds are: " , runtime.seconds());
-            telemetry.update();
 
-            if (runtime.seconds() >= 1.0) {
-                telemetry.addLine("seconds");
+            if (runtime.seconds() >= waitTime) {
                 telemetry.update();
                 stage = 2;
                 runtime.reset();
             }
-
         }
 
+
         if (stage == 2) {
-            telemetry.addLine("Stage 2");
-            handServo.setPosition(open);
-            telemetry.update();
-
-            if (runtime.seconds() >= 1.0) {
-                telemetry.addLine("seconds");
+            boolean detectObject = !sensor.getState();
+            if (detectObject) {
+                telemetry.addLine("Detected object");
                 telemetry.update();
-                stage = 3;
+                resetGripper();
+            } else {
+                telemetry.addLine("Not detected");
+                telemetry.update();
                 runtime.reset();
+                stage = 3;
             }
-
         }
 
         if (stage == 3) {
-            telemetry.addLine("Stage 3");
-            handServo.setPosition(close);
-
-            if (runtime.seconds() >= 1.0) {
-                telemetry.addLine("seconds");
                 telemetry.update();
-                stage = 1;
-                runtime.reset();
-            }
+                handServo.setPosition(open);
+
+                if (runtime.seconds() >= waitTime) {
+                    telemetry.update();
+                    resetGripper();
+                }
+
+
         }
 
 
-        //    telemetry.addLine("X is pressed.");
-//
-//    handServo.setPosition(close);
-//
-//    Thread.sleep(1000);
-//
-//    boolean detectObject = sensor.getState();
-//    telemetry.addData("Detected object", detectObject);
-//
-//    if (!detectObject) {
-//        handServo.setPosition(open);
-//        Thread.sleep(1000);
-//        handServo.setPosition(close);
-//        Thread.sleep(1000);
-//    }
-//    telemetry.update();
     }
 
+    public void resetGripper() throws InterruptedException {
+        stage = 1;
+        runtime = null;
+        isBusyGrabbing = false;
+    }
     public void dropTheBlock() throws InterruptedException {
         handServo.setPosition(open);
 
-        telemetry.addLine("Y is pressed.");
-        telemetry.update();
     }
 }
